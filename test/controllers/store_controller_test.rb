4448d5356d7f3d7e28deef8aa318fd5dc0dd9d97
Miguel Coronel <m.coronel.seg@gmail.com>
2015-09-15 14:34:32 -0500

    Add store index controller test

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,10 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '#columns #side a', minimium: 4
+    assert_select '#main .entry',     2
+    assert_select 'h3', 'Programming Ruby 1.9'
+    assert_select '.price', PRICE_PRESENTATION_REGEX
   end
 
 end
