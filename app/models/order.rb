a8a0e3f6278649268f91fc9a0250fb55be3cb073
Miguel Coronel <m.coronel.seg@gmail.com>
2015-09-19 01:10:44 -0500

    Add checkout funcionality

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,14 @@
 class Order < ActiveRecord::Base
+  has_many :line_items, dependent: :destroy
+
+  PAYMENT_TYPES = ["Check", "Credit card", "Purchase order"]
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
+
+  def add_line_items_from_cart cart
+    cart.line_items.each do |line_item|
+      line_item.cart_id = nil
+      line_items << line_item
+    end
+  end
 end
