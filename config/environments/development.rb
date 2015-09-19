2bd6ea9f36ca47e9177fa52039d953ed6a2f570c
Miguel Coronel <m.coronel.seg@gmail.com>
2015-09-19 01:57:15 -0500

    Configure local email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,15 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: "plain",
+    user_name: ENV["GMAIL_USERNAME"],
+    password:  ENV["GMAIL_PASSWORD"],
+    enable_starttls_auto: true
+  }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
